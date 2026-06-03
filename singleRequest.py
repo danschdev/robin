512f997304c1e061448827f21616520edb78335c
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-03 23:19:57 +0200

    Have gemma3 evaluate which model to use for a certain problem

--- singleRequest.py
+++ singleRequest.py
@@ -2,17 +2,18 @@
 import json
 import requests
 
-requestText = "Guten Morgen Sonnenschein, weck mich auf und komm herein!"
+requestText = "Hallo, lass uns ein bisschen plaudern!"
+prefaceText = "Hallo, ich habe folgende Anfrage erhalten. Bitte entscheide für mich, welches Sprachmodell diese am schnellsten und ressourcenschonendsten löst und gib mir nur den exakten Namen des Modells zurück, wie es in folgender Liste steht:"
+prefaceText += "gemma3:1b, gpt-oss:20b, llama3.1, qwen2; Hier die Anfrage: "
 url = "http://localhost:11434/api/chat"
 data = {
-    "model": "gpt-oss:20b",
+    "model": "gemma3:1b",
     "messages": [
         {
             "role": "user",
-            "content": requestText,
+            "content": prefaceText + requestText,
         }
     ],
-    "think": True,
 }
 print(requestText)
 with requests.post(url, data=json.dumps(data), stream=True) as stream:
@@ -22,14 +23,7 @@ with requests.post(url, data=json.dumps(data), stream=True) as stream:
     for line in stream.iter_lines():
         object = json.loads(line)
         message = object.get("message")
-        if message and "thinking" in message:
-            responseThoughts += object["message"]["thinking"]
-            print(object["message"]["thinking"], end="")
-        else:
-            if not finishedThinking:
-                finishedThinking = True
-                print("\n\n------------------------\n\n")
-            if message and message["role"] == "assistant":
-                responseText += object["message"]["content"]
-                if responseText:
-                    print(object["message"]["content"], end="")
+        if message and message["role"] == "assistant":
+            responseText += object["message"]["content"]
+            if responseText:
+                print(object["message"]["content"], end="")
