f4e09d700cf2fb8d80442e1e4877179eac877623
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-04 16:29:29 +0200

    Add database timestamp columns

--- setup/setupDatabase.py
+++ setup/setupDatabase.py
@@ -9,12 +9,14 @@ cursor = conn.cursor()
 
 cursor.execute("""
                CREATE TABLE IF NOT EXISTS requests (id INTEGER PRIMARY KEY AUTOINCREMENT,
-               content TEXT NOT NULL);
+               content TEXT NOT NULL,
+               created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
                """)
 cursor.execute("""
                CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY AUTOINCREMENT,
                content TEXT NOT NULL,
                request_id INTEGER,
+               created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                CONSTRAINT fk_requests
                FOREIGN KEY (request_id)
                REFERENCES requests(id))
