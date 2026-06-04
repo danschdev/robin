178c29b9abc46b0c41e18918c87dd61999386e32
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-04 11:45:21 +0200

    Create response table

--- setup/setupDatabase.py
+++ setup/setupDatabase.py
@@ -11,4 +11,12 @@ cursor.execute("""
                CREATE TABLE IF NOT EXISTS requests (id INTEGER PRIMARY KEY AUTOINCREMENT,
                content TEXT NOT NULL);
                """)
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY AUTOINCREMENT,
+               content TEXT NOT NULL,
+               request_id INTEGER,
+               CONSTRAINT fk_requests
+               FOREIGN KEY (request_id)
+               REFERENCES requests(id))
+               """)
 conn.close()
