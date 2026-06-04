2f6149c287b99a7a1e64a811332dcb6623f5c4ae
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-04 16:20:41 +0200

    Save request and response pair to database

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 import json
+from pathlib import Path
 import requests
+import sqlite3
+
+script_directory = Path(__file__).resolve().parent
+db = script_directory / "data" / "database.db"
+
+conn = sqlite3.connect(db)
+cursor = conn.cursor()
 
 print("Enter request: ")
 requestText = input()
+
+statement = """
+                INSERT INTO requests (content)
+               VALUES (?)
+               RETURNING id, content
+             """
+data = [requestText]
+
+response = cursor.execute(statement, data)
+
+requestId, requestText = response.fetchone()
+conn.commit()
+
 url = "http://localhost:11434/api/chat"
 thinkParameter = False
 data = {
@@ -34,3 +55,14 @@ with requests.post(url, data=json.dumps(data), stream=True) as stream:
                 responseText += object["message"]["content"]
                 if responseText:
                     print(object["message"]["content"], end="")
+
+statement = """
+               INSERT INTO responses (content, request_id)
+               VALUES (?, ?)
+               """
+data = [responseText, requestId]
+
+cursor.execute(statement, data)
+
+
+conn.commit()
