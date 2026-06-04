abd838df6b3ee10b323ed73f158411e0433e22f9
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-04 16:49:40 +0200

    Simplify code

--- main.py
+++ main.py
@@ -16,17 +16,12 @@ requestText = input()
 statement = """
                 INSERT INTO messages (content, role)
                VALUES (?,  "user")
-               RETURNING id, content
              """
-data = [requestText]
 
-response = cursor.execute(statement, data)
-
-requestId, requestText = response.fetchone()
+cursor.execute(statement, [requestText])
 conn.commit()
 
 url = "http://localhost:11434/api/chat"
-thinkParameter = False
 data = {
     "model": "gemma3:1b",
     "messages": [
@@ -35,26 +30,16 @@ data = {
             "content": requestText,
         }
     ],
-    "think": thinkParameter,
 }
 with requests.post(url, data=json.dumps(data), stream=True) as stream:
     responseThoughts = ""
     responseText = ""
-    finishedThinking = False
     for line in stream.iter_lines():
         object = json.loads(line)
         message = object.get("message")
-        if message and thinkParameter and "thinking" in message:
-            responseThoughts += object["message"]["thinking"]
-            print(object["message"]["thinking"], end="")
-        else:
-            if thinkParameter and not finishedThinking:
-                finishedThinking = True
-                print("\n\n------------------------\n\n")
-            if message and message["role"] == "assistant":
-                responseText += object["message"]["content"]
-                if responseText:
-                    print(object["message"]["content"], end="")
+        responseText += object["message"]["content"]
+        if responseText:
+            print(object["message"]["content"], end="")
 
 statement = """
                INSERT INTO messages (content, role)
