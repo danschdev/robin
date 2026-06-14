d2371674a49829b072b0d97d3a19ab04eaf4b98f
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-14 12:39:29 +0200

    Display chat messages from database in Flask

--- server/server.py
+++ server/server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from pandas import pandas
+from pathlib import Path
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,3 +9,31 @@ app = Flask(__name__)
 @app.route("/")
 def hallo():
     return "Hallo Flask"
+
+
+@app.route("/chat")
+def getChat():
+    script_directory = Path(__file__).resolve().parent.parent
+
+    print(script_directory)
+    db = script_directory / "data" / "database.db"
+    conn = sqlite3.connect(db)
+
+    statement = """
+                    SELECT created_at, role, content FROM messages
+                    ORDER BY created_at ASC
+                """
+
+    df = pandas.read_sql_query(statement, conn)
+
+    messages = []
+
+    for row in df.iterrows():
+        messages.append(
+            {
+                "role": row[1]["role"],
+                "content": row[1]["content"],
+            }
+        )
+
+    return messages
