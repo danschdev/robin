bcfcd1c5f5af6cd6d9a7bbd9ff804df997bcb4da
Daniel Schnelle <daniel_schnelle@web.de>
2026-06-14 22:43:17 +0200

    Add data seed script

--- setup/addActivityTable.py
+++ setup/addActivityTable.py
@@ -14,9 +14,4 @@ cursor.execute("""
                moving_time INTEGER);
                """)
 
-cursor.execute("""
-                INSERT INTO activities (start_date_local, distance, moving_time)
-               VALUES ("2018-02-16T06:52:54Z", 28099, 4207)
-               """)
-
 conn.commit()
